4acab9a22f277ae794cbf86c5e46b3ce051f3f2b
NextAbyss <63849033+NextAbyss@users.noreply.github.com>
2024-12-11 13:27:44 +0800

    Update shadowrocket_convert.py

--- Rules/shadowrocket_convert.py
+++ Rules/shadowrocket_convert.py
@@ -1,15 +1,19 @@
 import yaml
 import sys
+import os
 
 def process_line(line):
     # 删除前缀 '- '+.' 和处理后缀
     domain = line.strip("'").lstrip("- '+.")
     # 判断有多少个点来决定使用 DOMAIN 或 DOMAIN-SUFFIX
     if domain.count('.') == 1:
-        processed_line = f"DOMAIN,{domain},Proxy"
-    else:
         processed_line = f"DOMAIN-SUFFIX,{domain},Proxy"
+    else:
+        processed_line = f"DOMAIN,{domain},Proxy"
     return processed_line
+
+# 设置默认的输入目录路径
+directory_path = '/home/runner/work/Clash_Config/Clash_Config'
 
 def process_file(input_file):
     with open(input_file, 'r') as file:
@@ -21,7 +25,7 @@ def process_file(input_file):
         line = line.strip()
         
         # 忽略 payload 和注释行
-        if line.startswith("payload:") or line.startswith("##"):
+        if line.startswith("payload:") or line.startswith("#"):
             continue
         
         # 处理有效的域名行
@@ -33,7 +37,9 @@ def main(output_file, *input_files):
     all_results = []
 
     for input_file in input_files:
-        all_results.extend(process_file(input_file))
+        # 构建完整的文件路径
+        full_input_path = os.path.join(directory_path, input_file)
+        all_results.extend(process_file(full_input_path))
 
     # 写入输出文件
     with open(output_file, 'w') as outfile:
